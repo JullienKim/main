6fd9af31ffe6fca11fab51f346bc5766413334a5
JullienKim <jann21.llk@gmail.com>
2024-07-29 21:35:38 +0900

    240728

---
+++ 240729/01js/script.js
@@ -0,0 +1,59 @@
+// const test1 = "mon";
+// test1 = "tue";
+
+// 프로그램을 개발
+// -width 1920px
+// -슬라이드 개수 5개 고정
+
+// 스크롤을 내리게 되었을 때, 내려간 값에 따라서 무언가를 변경!!
+
+// let test2 = "mon";
+// test2 = "tue";
+
+// group pt
+// A 카테고리 안 상품개수
+// var category = 10;
+
+// B 장바구니 안 상품개수
+// var category = 8;
+
+// const x =10;
+// const z = x + y;
+// const y = 20;
+// console.log(z);
+
+// var x =10;
+// var z = x + y;
+// var y = 20;
+// console.log(z);
+// Not a Number
+
+// document.querySelector("#wrapper").addEventListener("click",() => {
+// });
+
+// const wrapper = document.querySelector("#wrapper");
+// console.log(wrapper);
+
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+// console.log(sum);
+
+// const str1 = "1";
+// const str2 = "2";
+
+// console.log(typeof str1);
+
+const num = 1000;
+const str = "원";
+
+const counter = num + str;
+console.log(typeof counter)
+
+const num1 =1000;
+const str1 ="2000";
+
+const counter1 = num1 + str1;
+console.log=(counter1);
+prompt("얼마룰 원하세요?")
