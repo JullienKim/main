194bdc9339287d4607e8b0c42ecd8583596fdb39
JullienKim <jann21.llk@gmail.com>
2024-08-07 22:01:24 +0900

    240807

---
+++ 240807/04js/script2.js
@@ -0,0 +1,12 @@
+//js 현시점의 날짜 정보 데이터를 생성해주는 함수
+// 년,월,일,시,분,코
+
+const today = new Date();
+const hrs = today.getHours();
+
+const container = document.querySelector("container");
+const newImg = document.createElement("img");
+
+newImg.src = hrs < 12 ? "./images/morning.jps" : "./img/afternoon.jpg";
+
+container.appendChild(newImg);
